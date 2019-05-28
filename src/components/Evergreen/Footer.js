d1c579a2107d348f9a873b7602e31dbb2bee9603
Jonathan Raxa <jonathanraxa@gmail.com>
2019-05-28 11:52:38 -0700

    fixed footer icon placement for mobile

--- src/components/Evergreen/Footer.js
+++ src/components/Evergreen/Footer.js
@@ -7,14 +7,25 @@ export default function Footer({ dark, id }) {
             <div className="section-content" id={id}>
                 <footer style={{ backgroundColor: 'inherit' }}>
                     <div className="container text-center">
-                        <a className="paddLeft2em" href={PERSONAL_LINKS.github} target="_blank"><i className="fab fa-github fa-2x" /></a>&nbsp;&nbsp;
-                        <a className="paddLeft2em" href={PERSONAL_LINKS.linkedin} target="_blank"><i className="fab fa-linkedin fa-2x" /></a>&nbsp;&nbsp;
-                        <a className="paddLeft2em" href={PERSONAL_LINKS.twitter} target="_blank"><i className="fab fa-twitter fa-2x" /></a> &nbsp;&nbsp;
-                        <a className="paddLeft2em" href={PERSONAL_LINKS.facebook} target="_blank"><i className="fab fa-facebook-f fa-2x" /></a>&nbsp;&nbsp;
-                        <a className="paddLeft2em" href={PERSONAL_LINKS.googleplus} target="_blank"><i className="fab fa-google-plus fa-2x" /></a>
+                    </div>
+                    <div class="row center-margin">
+                        <div className="col icon-col">
+                            <a className="" href={PERSONAL_LINKS.github} target="_blank"><i className="fab fa-github fa-2x" /></a>
+                        </div>
+                        <div className="col icon-col">
+                            <a className="" href={PERSONAL_LINKS.linkedin} target="_blank"><i className="fab fa-linkedin fa-2x" /></a>
+                        </div>
+                        <div className="col icon-col">
+                            <a className="" href={PERSONAL_LINKS.twitter} target="_blank"><i className="fab fa-twitter fa-2x" /></a> 
+                        </div>
+                        <div className="col icon-col">
+                            <a className="" href={PERSONAL_LINKS.facebook} target="_blank"><i className="fab fa-facebook-f fa-2x" /></a>
+                        </div>
+                        <div className="col icon-col">
+                            <a className="" href={PERSONAL_LINKS.googleplus} target="_blank"><i className="fab fa-google-plus fa-2x" /></a>
+                        </div>
                     </div>
                 </footer>
-
             </div>
         </div>
     );
